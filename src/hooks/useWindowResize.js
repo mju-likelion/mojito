7f5709549524875f60c477f3217a51ba041ee70f
Bhanjo <hanjo1515@naver.com>
2023-02-19 20:37:56 +0900

    feat: resize 커스텀 hook 제작 feat: banner 반응형 UI 적용

---
+++ src/hooks/useWindowResize.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useWindowReszie = () => {
+  const [windowSize, setWindowSize] = useState({ width: null, height: null });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({ width: window.innerWidth, height: window, innerHeight });
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return [windowSize, setWindowSize];
+};
+
+export default useWindowReszie;
--- src/pages/Main/Banner/index.js
+++ src/pages/Main/Banner/index.js
@@ -1,13 +1,26 @@
 import styled from 'styled-components';
 
 import Slogan from '../../../assets/images/Slogan';
+import useWindowReszie from '../../../hooks/useWindowResize';
 
 const Banner = () => {
+  const [size] = useWindowReszie();
+  const { width, height } = SloganSize(size);
   return (
     <Container>
-      <Slogan />
+      <Slogan width={width} height={height} />
     </Container>
   );
+};
+
+const SloganSize = size => {
+  if (size.width >= 1200) {
+    return { width: '1200', height: '370' };
+  } else if (size.width >= 600) {
+    return { width: '568', height: '176' };
+  } else {
+    return { width: '318', height: '100' };
+  }
 };
 
 /** @TODO hanjo: 모바일, 태블릿 스타일링 적용 필요 */
