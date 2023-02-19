cac9609a12d1da561d42111d2c3c05b82eec03f7
mywhzy <cmj2393a@naver.com>
2023-02-19 23:17:55 +0900

    chore: footer icon 추가 및 기본 프레임 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import Footer from './components/Footer';
 import Main from './pages/Main';
 import GlobalStyle from './styles/GlobalStyle';
 import { Theme } from './styles/Theme';
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Main />} />
         </Routes>
       </ThemeProvider>
+      <Footer />
     </BrowserRouter>
   );
 }
