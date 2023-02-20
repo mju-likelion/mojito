865538153a2da9c93331e689938c17760d42c38c
yhyem <yoonhyemin0120@gmail.com>
2023-02-21 02:45:56 +0900

    chore: 타이틀 버튼 반응형 스타일링

--- src/components/TitleButton.js
+++ src/components/TitleButton.js
@@ -15,11 +15,15 @@ const Title = styled.div`
   display: inline-block;
   color: ${({ theme }) => theme.colors.BLUE1};
   border: 1px solid ${({ theme }) => theme.colors.BLUE1};
-  border-radius: 30px;
+  border-radius: 100px;
   padding: 7px 30px;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 20px;
 
   @media ${({ theme }) => theme.devices.DESKTOP} {
     padding: 15px 46px;
+    font-size: 18px;
   }
 `;
 
