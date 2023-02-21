d0c365a3677c3cb3c0031e2e54c022cdfc699b9b
harksu <harksulim@naver.com>
2023-02-22 02:02:00 +0900

    feat: mobile view

--- src/pages/Main/OfficialApply/index.js
+++ src/pages/Main/OfficialApply/index.js
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 import Button from '../../../components/Button';
 
 const OfficialApply = () => {
+  // BreakLine에 관련된 태그 남용 부분은 추후 로직이 merge되면 리팩터링 하도록 하겠습니다.
   return (
     <Container>
       <ContentText>
-        도전을 두려워하지않고 어제보다 나은 오늘을 살고자 한다면,
-        <br /> 하던 일이 막히더라도 해결하고자 하는 승부욕을 가지고 있다면,
-        <br /> 할 땐 하고 놀 땐 제대로 노는 세상 멋있는 여러분을 기다리고 있겠습니다.
+        도전을 두려워하지않고 어제보다 나은 오늘을
+        <MobileBreakLine /> 살고자 한다면,
+        <NotMobileBreakLine /> 하던 일이 막히더라도 해결하고자 하는
+        <MobileBreakLine /> 승부욕을 가지고 있다면,
+        <NotMobileBreakLine /> 할 땐 하고 놀 땐 제대로 노는
+        <MobileBreakLine /> 세상 멋있는 여러분을 기다리고 있겠습니다.
         <ProposeText>저희와 함께 가실까요?</ProposeText>
       </ContentText>
       <ButtonBox>
@@ -25,6 +29,9 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 305px;
+  height: 311px;
+  margin: 80px auto 100px;
   @media ${({ theme }) => theme.devices.TABLET} {
     width: 412px;
     height: 287px;
@@ -41,11 +48,8 @@ const ContentText = styled.span`
   font-weight: 400;
   color: ${({ theme }) => theme.colors.GRAY2};
   text-align: center;
-
-  @media ${({ theme }) => theme.devices.TABLET} {
-    font-size: 14px;
-    line-height: 24px;
-  }
+  font-size: 14px;
+  line-height: 24px;
   @media ${({ theme }) => theme.devices.DESKTOP} {
     font-size: 20px;
     line-height: 32px;
@@ -56,11 +60,9 @@ const ProposeText = styled.p`
   margin-top: 30px;
   text-align: center;
   font-weight: 700;
+  font-size: 24px;
+  line-height: 30px;
   color: ${({ theme }) => theme.colors.WHITE};
-  @media ${({ theme }) => theme.devices.TABLET} {
-    font-size: 24px;
-    line-height: 30px;
-  }
   @media ${({ theme }) => theme.devices.DESKTOP} {
     font-size: 40px;
     line-height: 50px;
@@ -68,11 +70,26 @@ const ProposeText = styled.p`
 `;
 
 const ButtonBox = styled.div`
+  margin: 80px auto 125px;
   @media ${({ theme }) => theme.devices.TABLET} {
     margin: 80px auto 25px;
   }
   @media ${({ theme }) => theme.devices.DESKTOP} {
     margin: 100px auto 40px;
+  }
+`;
+
+const NotMobileBreakLine = styled.br`
+  display: none;
+  @media ${({ theme }) => theme.devices.TABLET} {
+    display: block;
+  }
+`;
+
+const MobileBreakLine = styled.br`
+  display: block;
+  @media ${({ theme }) => theme.devices.TABLET} {
+    display: none;
   }
 `;
 
