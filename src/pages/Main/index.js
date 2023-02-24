78ca3ed7f5ad87c54312e3812216cba47c64b016
mywhzy <cmj2393a@naver.com>
2023-02-24 21:10:43 +0900

    chore: ToastTest 파일 삭제

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,6 @@
 import About from './About';
 import Banner from './Banner';
 import FootPrint from './FootPrint';
-import ToastTest from './ToastTest';
 
 const Main = () => {
   return (
@@ -9,7 +8,6 @@ const Main = () => {
       <Banner />
       <About />
       <FootPrint />
-      <ToastTest />
     </>
   );
 };
