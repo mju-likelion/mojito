d51cace285c2ecdb5941ade48f0700969a6721e0
yhyem <yoonhyemin0120@gmail.com>
2023-02-25 02:17:38 +0900

    refactor: svg파일 png로 변경

--- src/pages/Main/About/AboutContent/AboutMJU.js
+++ src/pages/Main/About/AboutContent/AboutMJU.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
+import AboutMju1Mobile from '../../../../assets/images/AboutMju1Mobile.png';
+import AboutMju1PC from '../../../../assets/images/AboutMju1PC.png';
+import AboutMju1Tablet from '../../../../assets/images/AboutMju1Tablet.png';
+import AboutMju2Mobile from '../../../../assets/images/AboutMju2Mobile.png';
+import AboutMju2PC from '../../../../assets/images/AboutMju2PC.png';
+import AboutMju2Tablet from '../../../../assets/images/AboutMju2Tablet.png';
+import AboutMju3Mobile from '../../../../assets/images/AboutMju3Mobile.png';
+import AboutMju3PC from '../../../../assets/images/AboutMju3PC.png';
+import AboutMju3Tablet from '../../../../assets/images/AboutMju3Tablet.png';
 import ArticleImageCard from '../../../../components/ArticleImageCard';
 
 /** @TODO TABLE 버전 UI 추가하기 */
@@ -11,17 +20,17 @@ const AboutMJU = () => {
       <Article>
         <PcImages>
           {FILE_NAME.pc.map((item, idx) => (
-            <ArticleImageCard key={idx} imageName={item} />
+            <ArticleImageCard key={idx} image={item.image} title={item.title} text={item.text} />
           ))}
         </PcImages>
         <TabletImages>
           {FILE_NAME.tablet.map((item, idx) => (
-            <ArticleImageCard key={idx} imageName={item} />
+            <ArticleImageCard key={idx} image={item.image} title={item.title} text={item.text} />
           ))}
         </TabletImages>
         <MobileImages>
           {FILE_NAME.mobile.map((item, idx) => (
-            <ArticleImageCard key={idx} imageName={item} />
+            <ArticleImageCard key={idx} image={item.image} title={item.title} text={item.text} />
           ))}
         </MobileImages>
       </Article>
@@ -31,9 +40,57 @@ const AboutMJU = () => {
 
 /** @TODO 현재 태블릿, 모바일 버전 svg 컴포넌트 내용이 pc버전 내용임, 버전에 맞게 수정 필요 */
 const FILE_NAME = {
-  pc: ['AboutMju1PC', 'AboutMju2PC', 'AboutMju3PC'],
-  tablet: ['AboutMju1Tablet', 'AboutMju2Tablet', 'AboutMju3Tablet'],
-  mobile: ['AboutMju1Mobile', 'AboutMju2Mobile', 'AboutMju3Mobile'],
+  pc: [
+    {
+      image: AboutMju1PC,
+      title: '멋쟁이사자처럼 명지대(자연)은\n',
+      text: '2019년부터 시작되어 현재까지 5년간\n많은 학우들과 웹 개발을 함께 해오고 있습니다.',
+    },
+    {
+      image: AboutMju2PC,
+      title: '웹 서비스 제작을 위한 프로그래밍 언어부터 다양한\n프레임워크까지',
+      text: ' 직접 사용해보고 공부할 뿐만 아니라\n파트별 커리큘럼을 통해 지식이 실제 서비스로 연결될 수\n있도록 탄탄한 스터디 로드맵을 구축하고 있습니다.',
+    },
+    {
+      image: AboutMju3PC,
+      title: '',
+      text: '열정 가득한 운영진, 페이스메이커가 되어줄\n11기 회원들과 함께 1년간 웹 개발의 세상 속에서\n달릴 준비가 되었다면 지금 바로\n멋쟁이사자처럼 명지대 (자연)에 지원하세요.',
+    },
+  ],
+  tablet: [
+    {
+      image: AboutMju1Tablet,
+      title: '멋쟁이사자처럼 명지대(자연)은\n',
+      text: '2019년부터 시작되어 현재까지 5년간\n많은 학우들과 웹 개발을 함께 해오고 있습니다.',
+    },
+    {
+      image: AboutMju2Tablet,
+      title: '웹 서비스 제작을 위한 프로그래밍 언어부터\n다양한 프레임워크까지',
+      text: ' 직접 사용해보고 공부할 뿐만 아니라\n파트별 커리큘럼을 통해 지식이 실제 서비스로 연결될 수 있도록\n탄탄한 스터디 로드맵을 구축하고 있습니다.',
+    },
+    {
+      image: AboutMju3Tablet,
+      title: '',
+      text: '열정 가득한 운영진, 페이스메이커가 되어줄 11기 회원들과\n함께 1년간 웹 개발의 세상 속에서 달릴 준비가 되었다면\n지금 바로 멋쟁이사자처럼 명지대 (자연)에 지원하세요.',
+    },
+  ],
+  mobile: [
+    {
+      image: AboutMju1Mobile,
+      title: '멋쟁이사자처럼 명지대(자연)은\n',
+      text: '2019년부터 시작되어 현재까지 5년간 많은\n학우들과 웹 개발을 함께 해오고 있습니다.',
+    },
+    {
+      image: AboutMju2Mobile,
+      title: '웹 서비스 제작을 위한 프로그래밍 언어부터 다양한\n프레임워크까지',
+      text: ' 직접 사용해보고 공부할 뿐만 아니라 파트별\n커리큘럼을 통해 지식이 실제 서비스로 연결될 수 있도록\n탄탄한 스터디 로드맵을 구축하고 있습니다.',
+    },
+    {
+      image: AboutMju3Mobile,
+      title: '',
+      text: '열정 가득한 운영진, 페이스메이커가 되어줄 11기 회원들과\n함께 1년간 웹 개발의 세상 속에서 달릴 준비가 되었다면\n지금 바로 멋쟁이사자처럼 명지대 (자연)에 지원하세요.',
+    },
+  ],
 };
 
 const Wrapper = styled.div`
@@ -120,6 +177,7 @@ const TabletImages = styled.div`
 `;
 
 const MobileImages = styled.div`
+  margin: auto;
   @media ${({ theme }) => theme.devices.TABLET} {
     display: none;
   }
