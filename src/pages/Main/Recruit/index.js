3039b3524ffcdcfef6acb09f21cececa704e1230
mywhzy <cmj2393a@naver.com>
2023-02-27 05:55:46 +0900

    chore: padding 반응형 맞춰 수정

--- src/pages/Main/Recruit/index.js
+++ src/pages/Main/Recruit/index.js
@@ -18,11 +18,17 @@ const Recruit = () => {
 const Container = styled.div`
   width: 100%;
   margin: 0 auto;
-  padding: 160px 0;
+  padding: 80px 0;
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.GRAY4};
+  @media ${({ theme }) => theme.devices.TABLET} {
+    padding: 100px 0;
+  }
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    padding: 160px 0;
+  }
 `;
 
 export default Recruit;
