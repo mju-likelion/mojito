678cd6b8cfca61480a14f3f9943d9f2b6d612bff
ndaemy <yuyaebean@gmail.com>
2024-01-27 01:24:01 +0900

    chore: 오픈채팅방 관련 이스터에그 추가

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,3 +1,5 @@
+import EasterEgg from '../../components/EasterEgg';
+
 import About from './About';
 import Banner from './Banner';
 import Curriculum from './Curriculum';
@@ -14,6 +16,7 @@ const Main = () => {
       <Project />
       <Recruit />
       <OfficialApply />
+      <EasterEgg />
     </>
   );
 };
