62384a9c31c658bf342aea7dc192112a22b7c09b
zelkovaria <hyein396@gmail.com>
2024-02-19 14:57:43 +0900

    refactor: PartData 수정

--- src/pages/Main/Recruit/RecruitContent/RecruitPart/PartData.js
+++ src/pages/Main/Recruit/RecruitContent/RecruitPart/PartData.js
@@ -10,10 +10,10 @@ export const PART_DATA = {
   server: {
     key: 1,
     value: 'SERVER',
-    infoDesktop: '실제 서비스에 사용되는 API 서버를 설계하고,\nNestJS를 통해 실제 작동하는 서버로 구현/배포해요.',
-    infoTablet: '실제 서비스에 사용되는 API 서버를 설계하고, NestJS를 통해\n실제 작동하는 서버로 구현/배포해요.',
-    infoMobile: '실제 서비스에 사용되는 API 서버를 설계하고,\nNestJS를 통해 실제 작동하는 서버로 구현/배포해요.',
-    tool: 'Git, GitHub, TypeScript, NestJS, TypeORM, MySQL, AWS, Nginx',
+    infoDesktop: '실제 서비스에 사용되는 API 서버를 설계하고,\nSpring Boot를 통해 실제 작동하는 서버로 구현/배포해요.',
+    infoTablet: '실제 서비스에 사용되는 API 서버를 설계하고, Spring Boot를 통해\n실제 작동하는 서버로 구현/배포해요.',
+    infoMobile: '실제 서비스에 사용되는 API 서버를 설계하고,\nSpring Boot를 통해 실제 작동하는 서버로 구현/배포해요.',
+    tool: 'Git, GitHub, Java, SpringBoot, Redis, MySQL, AWS',
   },
   // design: {
   //   key: 2,
