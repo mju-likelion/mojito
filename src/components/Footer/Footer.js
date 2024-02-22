01e186d5cc7fbc7f594272cc42f370820986a875
zelkovaria <hyein396@gmail.com>
2024-02-22 13:42:20 +0900

    test: Footer에서 지원하기 사이트 이동 임시 삭제

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -22,8 +22,8 @@ const Footer = () => {
         />
       )}
       <RouteApplyBox href={process.env.REACT_APP_APPLY_URL} target="_blank" rel="noopener noreferrer">
-        <RouteApplyBtn>멋쟁이사자처럼 명지대(자연) 12기 APPLY</RouteApplyBtn>
-        <ArrowIcon />
+        {/* <RouteApplyBtn>멋쟁이사자처럼 명지대(자연) 12기 APPLY</RouteApplyBtn> */}
+        {/* <ArrowIcon /> */}
       </RouteApplyBox>
       <CopyrightBox>© 2024. LIKELION MJU All pictures cannot be copied without permission.</CopyrightBox>
     </FooterBox>
