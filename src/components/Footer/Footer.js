02ed1e1a0006f138d98d149bcaa5d2e88b41bc55
zelkovaria <hyein396@gmail.com>
2024-02-28 03:08:40 +0900

    fix: Footer의 no-unused-vars 처리

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import styled from 'styled-components';
 
-import { ReactComponent as ArrowIcon } from '../../assets/images/icon_footer_arrow.svg';
+// import { ReactComponent as ArrowIcon } from '../../assets/images/icon_footer_arrow.svg';
 
 import Toast from './../Toast';
 import FooterIcons from './FooterIcons';
@@ -65,19 +65,19 @@ const RouteApplyBox = styled.a`
   }
 `;
 
-const RouteApplyBtn = styled.button`
-  all: unset;
-  color: ${({ theme }) => theme.colors.GRAY2};
-  font-size: 12px;
-  line-height: 14px;
-  height: 20px;
-  text-align: center;
-  @media ${({ theme }) => theme.devices.DESKTOP} {
-    font-size: 14px;
-    line-height: 16px;
-    height: 30px;
-  }
-`;
+// const RouteApplyBtn = styled.button`
+//   all: unset;
+//   color: ${({ theme }) => theme.colors.GRAY2};
+//   font-size: 12px;
+//   line-height: 14px;
+//   height: 20px;
+//   text-align: center;
+//   @media ${({ theme }) => theme.devices.DESKTOP} {
+//     font-size: 14px;
+//     line-height: 16px;
+//     height: 30px;
+//   }
+// `;
 
 const CopyrightBox = styled.div`
   color: ${({ theme }) => theme.colors.GRAY3};
